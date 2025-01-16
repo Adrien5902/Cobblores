27b0dbde5566b728e0d7cc3eef2826f58fc8a924
Adrien5902 <adrien.m5902@gmail.com>
2025-01-16 19:46:24 +0100

    Initial Commit 🚀

---
+++ src/client/java/net/ddns/adrien5902/cobblores/client/CobbloresClient.java
@@ -0,0 +1,10 @@
+package net.ddns.adrien5902.cobblores.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobbloresClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/ddns/adrien5902/cobblores/Cobblores.java
@@ -0,0 +1,10 @@
+package net.ddns.adrien5902.cobblores;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cobblores implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
